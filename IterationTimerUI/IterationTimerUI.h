487ef35119f3179f415a3d3b8d7d723f024e5b82
hal1437 <halstroemeria@gmail.com>
2021-04-07 15:33:25 +0900

    IterationTimerUI追加

---
+++ IterationTimerUI/IterationTimerUI.h
@@ -0,0 +1,18 @@
+//
+//  IterationTimerUI.h
+//  IterationTimerUI
+//
+//  Created by hal1437 on 2021/04/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IterationTimerUI.
+FOUNDATION_EXPORT double IterationTimerUIVersionNumber;
+
+//! Project version string for IterationTimerUI.
+FOUNDATION_EXPORT const unsigned char IterationTimerUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IterationTimerUI/PublicHeader.h>
+
+
