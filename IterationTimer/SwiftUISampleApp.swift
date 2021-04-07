53f8cae3a50b822cde5406c1da60336fe4a78ccb
hal1437 <halstroemeria@gmail.com>
2021-04-07 15:14:53 +0900

    SwiftUIに変更

---
+++ IterationTimer/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IterationTimer.swift
+//  IterationTimer
+//
+//  Created by hal1437 on 2021/04/07.
+//
+
+import SwiftUI
+
+@main
+struct IterationTimer: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
