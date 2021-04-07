768174703f5ca29f39d447d578cffc00446d8421
hal1437 <halstroemeria@gmail.com>
2021-04-07 15:14:43 +0900

    ModelとTests追加

---
+++ IterationTimerModel/IterationTimerModel.h
@@ -0,0 +1,18 @@
+//
+//  IterationTimerModel.h
+//  IterationTimerModel
+//
+//  Created by hal1437 on 2021/04/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IterationTimerModel.
+FOUNDATION_EXPORT double IterationTimerModelVersionNumber;
+
+//! Project version string for IterationTimerModel.
+FOUNDATION_EXPORT const unsigned char IterationTimerModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IterationTimerModel/PublicHeader.h>
+
+
---
+++ IterationTimerModel/SampleFunction.swift
@@ -0,0 +1,12 @@
+//
+//  SampleFunction.swift
+//  IterationTimerModel
+//
+//  Created by hal1437 on 2021/04/07.
+//
+
+import Foundation
+
+func sum(a: Int, b: Int) -> Int {
+    return a + b
+}
