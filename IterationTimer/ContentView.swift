0f3dfae19634b54bbd07088b39be822f9b1ed48a
hal1437 <halstroemeria@gmail.com>
2021-04-13 01:45:10 +0900

    LazyVGrid設置

--- IterationTimer/ContentView.swift
+++ IterationTimer/ContentView.swift
@@ -9,20 +9,36 @@ import SwiftUI
 import IterationTimerUI
 
 struct ContentView: View {
+    
+    private let mock = [Drawable(), Drawable(), Drawable(), Drawable(), Drawable(),
+                        Drawable(), Drawable(), Drawable(), Drawable(), Drawable()]
+    
     var body: some View {
-        TimerCard(category: .game, title: "SampleTitle", drawable: Drawable())
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 300))]) {
+                ForEach(mock) {
+                    TimerCard(category: .game, title: "SampleTitle", drawable: $0)
+                        .padding()
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let devices = ["iPhone 8", "iPad Air (4th generation)"]
+        ForEach(devices, id: \.self) { device in
+            ContentView()
+                .previewDevice(.init(rawValue: device))
+                .previewDisplayName(device)
+        }
     }
 }
 
 // FIXME: Mock用構造体
-private struct Drawable: TimerDrawable {
+private struct Drawable: TimerDrawable, Identifiable {
+    var id = UUID()
     var startTime = Date()
     var currentTime = Date()
     var endTime = Date(timeIntervalSinceNow: TimeInterval(60))
