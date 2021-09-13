bb7e24469f53ae2e6ac4330acee375ea6c23b94d
hal1437 <halstroemeria@gmail.com>
2021-09-14 01:23:24 +0900

    ウィジェット更新契機を追加

--- IterationTimer/Main.swift
+++ IterationTimer/Main.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 @main
 struct Main: App {
     var body: some Scene {
         WindowGroup {
             TimerListsView()
+                .onAppear {
+                    WidgetCenter.shared.reloadAllTimelines()
+                }
         }
     }
 }
