bbfab421b9f199465bc65f1551f0232879c4f804
hal1437 <halstroemeria@gmail.com>
2021-09-19 19:47:28 +0900

    ウィジェット更新頻度を15分に設定

--- IterationTimerWidget/IterationTimerWidget.swift
+++ IterationTimerWidget/IterationTimerWidget.swift
@@ -21,15 +21,13 @@ struct Provider: IntentTimelineProvider {
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
+        let entries = (0 ..< 6)
+            .map { $0 * 15 }
+            .map { minuteOffset -> SimpleEntry in
+                let entryDate = Calendar.current.date(byAdding: .minute, value: minuteOffset, to: currentDate)!
+                return SimpleEntry(date: entryDate, configuration: configuration)
+            }
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
