2dbe6db1168fe99c045f9a46f2df942826ad572a
hal1437 <halstroemeria@gmail.com>
2022-06-14 02:51:38 +0900

    DurationPicker実装

---
+++ IterationTimerUI/Components/Inputs/DurationPicker.swift
@@ -0,0 +1,43 @@
+//
+//  DurationPicker.swift
+//  IterationTimerUI
+//
+//  Created by hal1437 on 2022/06/14.
+//
+
+import SwiftUI
+
+struct DurationPicker: View {
+    @Binding public var duration: TimeInterval
+    
+    let timeSequence = (1...119).map { TimeInterval($0 * 30) }
+    
+    var body: some View {
+        Picker("", selection: $duration) {
+            ForEach(timeSequence, id:\.self) { i in
+                Text(convertLocalize(second: i)).tag(TimeInterval(i))
+            }
+        }
+            .pickerStyle(MenuPickerStyle())
+            .font(.body)
+            .padding(.horizontal, 4)
+            .background(.secondarySystemBackground)
+            .cornerRadius(4)
+    }
+    
+    func convertLocalize(second: TimeInterval) -> String {
+        let f = DateComponentsFormatter()
+        f.unitsStyle = .short
+        f.allowedUnits = [.minute, .second]
+        
+        return f.string(from: TimeInterval(second))!
+    }
+}
+
+struct DurationPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        Form {
+            DurationPicker(duration: Binding<TimeInterval>(get: { 0 }, set: { _ in }))
+        }
+    }
+}
