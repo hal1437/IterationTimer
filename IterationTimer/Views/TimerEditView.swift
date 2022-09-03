48a96c5941f1326aafaa769de43304c3869f97c5
hal1437 <halstroemeria@gmail.com>
2022-09-03 18:03:57 +0900

    タイマー編集のViewを関数に分割

--- IterationTimer/Views/TimerEditView.swift
+++ IterationTimer/Views/TimerEditView.swift
@@ -37,72 +37,12 @@ struct TimerEditView: View {
             VStack(alignment: .center, spacing: 0) {
                 TimerCard(drawable: IterationTimerDrawable(timer: viewModel.currentTimer, date: Date())).padding()
                     .background(Color(UIColor.systemGroupedBackground))
+
                 Form {
-                    Section(header: Text("TimerEditStaminaSection")) {
-                        HStack {
-                            Text("TimerEditCurrentValue")
-                            Spacer()
-                            NumberPicker(max: viewModel.currentTimer.settings.maxStamina, number: $viewModel.input.currentStamina)
-                        }
-
-                        StaminaQuickAccess(text: "クエスト1", count: -30) {
-                            print("クエスト1")
-                        }
-                        StaminaQuickAccess(text: "クエスト2", count: 60) {
-                            print("クエスト2")
-                        }
-                    }
-                    
-                    Section {
-                        NavigationLink("TimerEditTimerSetting", destination: TimerSettingView(settings: $viewModel.input.settings)
-                            )
-                    }
-                    
-//                    Section {
-//                        Picker("TimerEditNotification", selection: $viewModel.timer.settings.notification) {
-//                            ForEach(TimerEditViewModel.NotificationSelection.allCases, id: \.self) {
-//                                Text($0.title)
-//                            }
-//                        }
-//
-//                        switch $viewModel.timer.settings.notification {
-//                        case .never: EmptyView()
-//                        case .on:
-//                            HStack {
-//                                Text("TimerEditNotificationOn")
-//                                    .frame(width: 100, alignment: .leading)
-//                                Spacer()
-////                                TextField("0", text: $viewModel.input.notification.on)
-////                                    .keyboardType(.numberPad)
-////                                    .multilineTextAlignment(.trailing)
-//                            }
-//                        case .completion:
-//                            HStack {
-//                                Text("TimerEditNotificationCompleteBefore")
-//                                    .frame(width: 100, alignment: .leading)
-//                                Spacer()
-////                                TextField("0", text: $viewModel.input.notification.completion)
-////                                    .keyboardType(.numberPad)
-////                                    .multilineTextAlignment(.trailing)
-//                            }
-//                        }
-//                    }
-//                    .disabled(!viewModel.isEnableNotification)
-
-                    Section {
-                        Button("TimerEditDeleteButton", role: .destructive) {
-                            self.showingDeleteAlert = true
-                        }
-                        .actionSheet(isPresented: $showingDeleteAlert) {
-                            ActionSheet(title: Text("TimerEditDeleteConfirm"), buttons: [
-                                .destructive(Text("TimerEditDelete"), action: {
-                                      viewModel.delete()
-                                      self.presentationMode.wrappedValue.dismiss()
-                                }),
-                                .cancel()]
-                            )
-                        }
-                    }
+                    StaminaSettingSection()
+                    TimerSettingSection()
+                    NotificationSettingSection()
+                    DeleteTmerSection()
                 }
             }
             .navigationBarTitle(NSLocalizedString("TimerEditTitle", comment: ""), displayMode: .inline)
@@ -115,6 +55,85 @@ struct TimerEditView: View {
             )
         }
     }
+    
+    @ViewBuilder
+    fileprivate func StaminaSettingSection() -> some View {
+        Section(header: Text("TimerEditStaminaSection")) {
+            HStack {
+                Text("TimerEditCurrentValue")
+                Spacer()
+                NumberPicker(max: viewModel.currentTimer.settings.maxStamina, number: $viewModel.input.currentStamina)
+            }
+            
+            StaminaQuickAccess(text: "クエスト1", count: -30) {
+                print("クエスト1")
+            }
+            StaminaQuickAccess(text: "クエスト2", count: 60) {
+                print("クエスト2")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func TimerSettingSection() -> some View {
+        Section {
+            NavigationLink("TimerEditTimerSetting", destination: TimerSettingView(settings: $viewModel.input.settings)
+            )
+        }
+    }
+    
+    @ViewBuilder
+    private func NotificationSettingSection() -> some View {
+        Section {
+//            Picker("TimerEditNotification", selection: $viewModel.timer.settings.notification) {
+//                ForEach(TimerEditViewModel.NotificationSelection.allCases, id: \.self) {
+//                    Text($0.title)
+//                }
+//            }
+//
+//            switch $viewModel.timer.settings.notification {
+//            case .never: EmptyView()
+//            case .on:
+//                HStack {
+//                    Text("TimerEditNotificationOn")
+//                        .frame(width: 100, alignment: .leading)
+//                    Spacer()
+//                                    TextField("0", text: $viewModel.input.notification.on)
+//                                        .keyboardType(.numberPad)
+//                                        .multilineTextAlignment(.trailing)
+//                }
+//            case .completion:
+//                HStack {
+//                    Text("TimerEditNotificationCompleteBefore")
+//                        .frame(width: 100, alignment: .leading)
+//                    Spacer()
+//                                    TextField("0", text: $viewModel.input.notification.completion)
+//                                        .keyboardType(.numberPad)
+//                                        .multilineTextAlignment(.trailing)
+//                }
+//            }
+        }
+        .disabled(!viewModel.isEnableNotification)
+    }
+
+    @ViewBuilder
+    fileprivate func DeleteTmerSection() -> some View {
+        Section {
+            Button("TimerEditDeleteButton", role: .destructive) {
+                self.showingDeleteAlert = true
+            }
+            .actionSheet(isPresented: $showingDeleteAlert) {
+                ActionSheet(title: Text("TimerEditDeleteConfirm"), buttons: [
+                    .destructive(Text("TimerEditDelete"), action: {
+                        viewModel.delete()
+                        self.presentationMode.wrappedValue.dismiss()
+                    }),
+                    .cancel()]
+                )
+            }
+        }
+    }
+
 }
 
 extension TimerEditViewModel.NotificationSelection {
